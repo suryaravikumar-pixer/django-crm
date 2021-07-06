dbe3f8fe661f5a12775e226091dbbe4478b0bf1c
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-07-06 15:14:11 +0530

    second commit

---
+++ crm/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+# from leads.views import home_page
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('leads/', include('leads.urls', namespace="leads")),
+    
+    
+]
